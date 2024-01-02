c580bc49c2aa3616b60138b87770a0ab848fbd95
zhaowenzhe <1553126902@qq.com>
2024-01-02 21:12:20 +0800

    ✨ feat: 播放条显示时间

--- src/component/Play.tsx
+++ src/component/Play.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { Card, Slider } from 'antd';
-import { APlayer } from "./Player";
 import "aplayer-react/dist/index.css";
 import { makeStyles } from '@mui/styles';
 import { ReactComponent as IconPlay } from "../assets/play.svg";
@@ -9,25 +8,13 @@ import { ReactComponent as IconPause } from "../assets/pause.svg";
 // api and interface 
 import type { EpisodeResponse } from "../api/listenadmin/episode/type";
 
+import { useLocation, useParams, useSearchParams } from 'react-router-dom';
 
 
 import "./Play.scss";
 import { useSyncExternalStore } from 'react';
-const useStyles = makeStyles({
-  selected: {
-    backgroundColor: '#4CAF50', // 设置选中状态的背景颜色
-    color: '#FFFFFF', // 设置选中状态的文本颜色
-  },
-  unselected: {
-    backgroundColor: '#CCCCCC', // 设置未选中状态的背景颜色
-    color: '#000000', // 设置未选中状态的文本颜色
-  },
-  globalClass: {
-    // 添加你的样式规则
-    position: 'static'
-  },
-});
-const mUrl = 'http://radio-upyun.test.upcdn.net/test/2023/12/18/4f8ca524691306871ad9eca1826adbe0636aa063957cddc9d540c3d3cd2c8b3a/%E5%8E%86%E5%B9%B4%E7%9C%9F%E9%A2%98(%E4%B8%80).m4a';
+import { formatAudioDuration } from '../utils/time';
+
 
 // const PlayerApp: React.FC<{ traceList: Track[], detailEpisode: EpisodeResponse }> = ({ traceList, detailEpisode }) => {
 const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> = ({ traceList, detailEpisode }) => {
@@ -36,7 +23,6 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
   const [progress, setProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isSliderChange, setIsSliderChange] = useState(false);
-  const [isChanging, setIsChanging] = useState(false);
   const [scroll, setScroll] = useState(0);
   const [lyric, setLyric] = useState<any>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(-1);
@@ -44,6 +30,8 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const audioRef = useRef<any>();
   const lyricContainerRef = useRef<any>();
+  let [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
     // 禁止滚动
     lyricContainerRef.current?.addEventListener('mousewheel', (e: Event) => {
@@ -51,14 +39,13 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
     })
   }, [])
   useEffect(() => {
-    audioRef.current.src = mUrl;
     let arr = detailEpisode?.sentences;
     setLyric(arr);
+    audioRef.current.src = searchParams.get("audioUrl");
   }, [detailEpisode?.sentences])
   const handlePlay = () => {
     setIsPlaying(!isPlaying);
     isPlaying ? handleStop() : audioRef.current.play().then((res: any) => {
-      console.log(res, 1234);
       // 返回成功 播放
       // setIsPlaying(true);
     }).catch((err: any) => {
@@ -70,89 +57,34 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
   const handleStop = () => {
     audioRef.current.pause()
   }
-  const handleTime = () => {
-    audioRef.current.currentTime = 104;
-  }
   // 由 currentTime 指定的时间更新。
   const timeUpData = (e: any) => {
+    const currentTime = e.target.currentTime;
     if (!isSliderChange) {
-      const progress = Math.ceil(e.target.currentTime);
+      setCurrentTime(currentTime);
       setProgress(progress);
     }
+    setCurrentTime(currentTime);
     const currentTimeUpData = e.target.currentTime;
     let curIndex = currentIndex != -1 ? currentIndex : 0;
-    let startTime = lyric?.[curIndex]?.startTime;
-    let endTime = lyric?.[curIndex]?.endTime;
     // 当前时间大于开始时间并且小于结束时间
-    lyric.forEach((item: { startTime: number; endTime: number; }, idx: number) => {
+    lyric?.forEach((item: { startTime: number; endTime: number; }, idx: number) => {
       if (currentTimeUpData > item.startTime && currentTimeUpData < item.endTime) {
         handleScroll(idx);
         setCurrentIndex(idx + 1);
       }
     })
-    if (endTime > currentTimeUpData && currentTimeUpData > startTime && currentIndex != null) {
-      handleScroll(curIndex);
-      setCurrentIndex(curIndex + 1);
-    }
   }
-  const currentTime1 = useSyncExternalStore(
-    useCallback(
-      (onStoreChange: () => void) => {
-        audioRef.current?.addEventListener("timeupdate", onStoreChange);
-
-        return () => {
-          audioRef.current?.removeEventListener(
-            "timeupdate",
-            onStoreChange
-          );
-        };
-      },
-      [audioRef]
-    ),
-    () => {
-      if (!audioRef.current) {
-        return undefined;
-      }
-      return Math.round(audioRef.current.currentTime);
-    },
-    () => undefined
-  );
   const sliderChange = (e: any) => {
     audioRef.current.play();
     setIsPlaying(true);
-    console.log(e, 'slider');
     setProgress(e);
     setIsSliderChange(true);
-    audioRef.current.currentTime = (detailEpisode?.durationInSecond / detailEpisode?.durationInSecond) * e;
+    audioRef.current.currentTime = e;
   }
   const sliderChangeComplete = (e: any) => {
     setIsSliderChange(false);
   }
-  // const timeUpdata = (e) => {
-  //   const currentTimeUpdata = e.target.currentTime;
-  //   if (!isChanging) {
-  //     setCurrentTime(currentTimeUpdata * detailEpisode?.durationInSecond);
-  //     setProgress(currentTimeUpdata * detailEpisode?.durationInSecond / duration * 100);
-  //   }
-  //   // 获取歌词
-  //   let i = 0;
-  //   for ( ; i < lyricList.length; i++){
-  //     let lyricItem = lyricList[i];
-  //     if (currentTimeUpdata * detailEpisode?.durationInSecond < lyricItem.time) {
-  //       break;
-  //     }
-  //   }
-  //   if (currentLyricIndex !== i - 1) {
-  //     dispatch(changeCurrentLyricIndexAction(i - 1));
-  //     const content = lyricList[i - 1] && lyricList[i - 1].content;
-  //     message.open({
-  //       key: "lyric",
-  //       content: content,
-  //       duration: 0,
-  //       className: "lyric-class"
-  //     })
-  //   }
-  // }
   // 处理歌曲播放结束
   const handleMusicEnded = () => { }
 
@@ -174,15 +106,6 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
   }
   return (
     <div>
-      {/* <APlayer audio={{
-        name: undefined,
-        artist: undefined,
-        url: mUrl,
-        cover: undefined,
-        lrc: undefined,
-        theme: undefined,
-        type: undefined
-      }} /> */}
       <Card style={{ padding: '50px 100px', display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
         <div>
           <div className="title">原文</div>
@@ -194,25 +117,6 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
             }</div>
         </div>
       </Card>
-      <Card >
-        {/* <APlayer
-          audio={{
-            name: "Dancing with my phone",
-            artist: "HYBS",
-            url: "https://music.163.com/song/media/outer/url?id=1969744125",
-          }}
-          autoPlay
-        /> */}
-        <button onClick={() => handlePlay()}>播放</button>
-        <button onClick={() => handleStop()}>暂停</button>
-        <button onClick={() => handleTime()}>跳过开头</button>
-
-        <audio controls
-          ref={audioRef}
-          onTimeUpdate={e => timeUpData(e)}
-          onEnded={e => handleMusicEnded()}
-        />
-      </Card>
       <div className="play-control-bar">
         <div className='play-pic-container' onClick={() => handlePlay()}>
           <img src="http://radio-upyun.test.upcdn.net/images/category/backiee-205922.jpg" alt="" />
@@ -221,7 +125,7 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
           </div>
         </div>
         <div className="play-progress">
-          <div className="play-title">标题</div>
+          <div className="play-title">{detailEpisode?.name?.chinese}</div>
           <div className="progress">
             <Slider defaultValue={30}
               value={progress}
@@ -235,10 +139,18 @@ const PlayerApp: React.FC<{ traceList: any[], detailEpisode: EpisodeResponse }> 
           </div>
         </div>
         <div className="paly-control-btn">
-          按钮
+          <div className="time">
+            <span className="now-time">{formatAudioDuration(currentTime)}</span>
+            <span className="divider">/</span>
+            <span className="duration">{formatAudioDuration(detailEpisode?.durationInSecond)}</span>
+          </div>
         </div>
       </div>
-
+      <audio
+        ref={audioRef}
+        onTimeUpdate={e => timeUpData(e)}
+        onEnded={e => handleMusicEnded()}
+      />
     </div>
   );
 }
--- src/utils/time.tsx
+++ src/utils/time.tsx
@@ -1,18 +1,39 @@
 //封装一个函数:获取一个结果:当前早上|上午|下午|晚上
 export const getTime = () => {
-    let message = ''
-    //通过内置构造函数Date
-    const hours = new Date().getHours()
-    //情况的判断
-    if (hours <= 9) {
-      message = '早上'
-    } else if (hours <= 12) {
-      message = '上午'
-    } else if (hours <= 18) {
-      message = '下午'
-    } else {
-      message = '晚上'
-    }
-    return message
+  let message = ''
+  //通过内置构造函数Date
+  const hours = new Date().getHours()
+  //情况的判断
+  if (hours <= 9) {
+    message = '早上'
+  } else if (hours <= 12) {
+    message = '上午'
+  } else if (hours <= 18) {
+    message = '下午'
+  } else {
+    message = '晚上'
   }
-  
+  return message
+}
+
+export function formatAudioDuration(seconds: number | undefined) {
+  if (typeof seconds === "undefined") {
+    return "--:--";
+  }
+  if (isNaN(seconds)) {
+    return "00:00";
+  }
+  const pad0 = (num: number) => {
+    return num < 10 ? "0" + num : "" + num;
+  };
+
+  const min = Math.trunc(seconds / 60);
+  const sec = Math.trunc(seconds - min * 60);
+  const hours = Math.trunc(min / 60);
+  const minAdjust = Math.trunc(
+    seconds / 60 - 60 * Math.trunc(seconds / 60 / 60)
+  );
+  return seconds >= 3600
+    ? pad0(hours) + ":" + pad0(minAdjust) + ":" + pad0(sec)
+    : pad0(min) + ":" + pad0(sec);
+}
